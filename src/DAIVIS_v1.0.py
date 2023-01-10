356532b7731f52ec9586cf4e7ba4887bc1f81ce6
HiMyNameIsDavidKim <rkfka1401@gmail.com>
2023-01-10 20:27:53 +0900

    1/10 hello daivis

--- src/DAIVIS_v1.0.py
+++ src/DAIVIS_v1.0.py
@@ -4,17 +4,19 @@ from gtts import gTTS
 from playsound import playsound
 
 
-class AI_Speaker(object):
+class DAIVIS(object):
     def __init__(self):
-        global fname_init, fname_ans
+        global fname_init, fname_ans, path_stock
         fname_init = './save/init.mp3'
         fname_ans = './save/ans.mp3'
+        path_stock = '/Users/davidkim/PycharmProjects/pythonProject/auto_stock_report/report/stock_report_23-01-10 .xlsx'
         self.shutdown = None
 
     def process(self):
         r = sr.Recognizer()
         m = sr.Microphone()
         self.speak('Welcome home Master.', fname_init)
+        # auto insight report, stock report, weather report
         self.shutdown = r.listen_in_background(m, self.listen)
 
     def listen(self, recognizer, audio):
@@ -28,28 +30,39 @@ class AI_Speaker(object):
             print(f'RequestError: {e}')
 
     def answer(self, text):
-        if 'hello' in text:
+        if 'daivis' == text:
             answer_text = 'yes Master.'
-        elif 'weather' in text:
-            answer_text = "The temperature in Seoul is 20 degrees. It will be sunny day."
         elif 'thank' in text:
             answer_text = 'My pleasure.'
         elif 'shutdown' in text:
             answer_text = 'See you Master.'
             self.shutdown(wait_for_stop=False)
+        elif 'stock' in text:
+            answer_text = self.stock_report()
+        elif 'weather' in text:
+            answer_text = 'weather'
         else:
             answer_text = 'Please speak again.'
         self.speak(answer_text, fname_ans)
 
     def speak(self, text, fname):
-        print('[AI]: ' + text)
+        print('[DAIVIS]: ' + text)
         tts = gTTS(text=text, lang='en')
         tts.save(fname)
         playsound(fname)
 
+    def stock_report(self):
+        asnwer = f''
+        if os.path.isfile(path_stock):
+          asnwer = f''
+        else:
+            # report analyze file run
+            asnwer = f'' + f'' + f''
+        return asnwer
+
 
 if __name__ == '__main__':
-    ai_sp = AI_Speaker()
-    ai_sp.process()
+    daivis = DAIVIS()
+    daivis.process()
     while True:
         time.sleep(0.1)
